7b3d8d2ba4fa1c96ac741ded273ab5c1c87de4dc
Andy Park <apark1214@gmail.com>
2021-02-01 19:04:55 -0800

    added empty map

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
 import Home from './pages/home';
 import LoginPage from './pages/logInPage';
+import {parseRoute} from './lib'
+
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+  componentDidMount() {
+
+    window.addEventListener('hashchange', () => {
+      const changedHash = window.location.hash;
+      const parsed = parseRoute(changedHash)
+      console.log(parsed);
+      this.setState({ route: parsed })
+
+    })
+  }
+
+  renderPage(){
+    const {route} = this.state;
+    if(route.path ==='home'){
+      return <Home />
+    }else if(route.path===''){
+      return <LoginPage />
+    }
+  }
+
   render() {
-    return <LoginPage />;
+    return <div>{this.renderPage()}</div>
+
   }
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Nav from '../components/navigationTop';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
-export default function Home(props) {
-  return (
-    <>
-    </>
-  );
+export default class Home extends React.Component{
+  render(){
+    const position = [35.0522, -118.2437]
+    return (
+      <MapContainer className="leaflet" center={[35.0522, -118.2437]} zoom={4} minZoom={3} maxBoundsViscosity={1.0} worldCopyJump={true} zoomControl={false} >
+                <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" zoomControl={false} />
+                {/* <ZoomControl zoomInText="🔍" zoomOutText="🌎" style={"zoom"}/> */}
+              </MapContainer >
+
+    )
+  }
 }
