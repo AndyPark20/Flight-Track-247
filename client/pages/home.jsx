4d2ed5abf35de0ce973168d0968e81c5345473f5
Andy Park <apark1214@gmail.com>
2021-02-01 19:12:41 -0800

    added a top nav bar

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 import Nav from '../components/navigationTop';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
-export default class Home extends React.Component{
-  render(){
-    const position = [35.0522, -118.2437]
+export default class Home extends React.Component {
+  render() {
     return (
-      <MapContainer className="leaflet" center={[35.0522, -118.2437]} zoom={4} minZoom={3} maxBoundsViscosity={1.0} worldCopyJump={true} zoomControl={false} >
-                <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" zoomControl={false} />
-                {/* <ZoomControl zoomInText="🔍" zoomOutText="🌎" style={"zoom"}/> */}
-              </MapContainer >
-
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+              <Nav />
+          </div>
+          <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <MapContainer className="leaflet" center={[35.0522, -118.2437]} zoom={4} minZoom={3} maxBoundsViscosity={1.0} worldCopyJump={true} zoomControl={false} >
+              <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" zoomControl={false} />
+            </MapContainer >
+          </div>
+          <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <Nav />
+          </div>
+        </div>
+      </div>
     )
   }
 }
