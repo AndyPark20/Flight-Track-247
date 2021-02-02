6cf0a32e94b57bd35d7d1740a57a1e81d576777f
Andy Park <apark1214@gmail.com>
2021-02-02 11:14:18 -0800

    added error-middleware

---
+++ server/client-error.js
@@ -0,0 +1,10 @@
+import { Children } from "react";
+
+class ClientError {
+  constructor(status, message) {
+    this.status = status;
+    this.message = message;
+  }
+}
+
+module.exports = ClientError;
