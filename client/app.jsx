f7226ffec5c7f917aa0d593e5437ff35fe30aca6
Andy Park <apark1214@gmail.com>
2021-02-02 10:21:30 -0800

    deleted console.log

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Home from './pages/home';
 import LoginPage from './pages/logInPage';
-import {parseRoute} from './lib'
-
+import { parseRoute } from './lib';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -11,28 +10,28 @@ export default class App extends React.Component {
       route: parseRoute(window.location.hash)
     };
   }
+
   componentDidMount() {
 
     window.addEventListener('hashchange', () => {
       const changedHash = window.location.hash;
-      const parsed = parseRoute(changedHash)
-      console.log(parsed);
-      this.setState({ route: parsed })
+      const parsed = parseRoute(changedHash);
+      this.setState({ route: parsed });
 
-    })
+    });
   }
 
-  renderPage(){
-    const {route} = this.state;
-    if(route.path ==='home'){
-      return <Home />
-    }else if(route.path===''){
-      return <LoginPage />
+  renderPage() {
+    const { route } = this.state;
+    if (route.path === 'home') {
+      return <Home />;
+    } else if (route.path === '') {
+      return <LoginPage />;
     }
   }
 
   render() {
-    return <div className="mainContainer">{this.renderPage()}</div>
+    return <div className="mainContainer">{this.renderPage()}</div>;
 
   }
 }
