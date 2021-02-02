ef32dc591745edc0bc8a243affce9cc466f58182
Andy Park <apark1214@gmail.com>
2021-02-02 15:22:02 -0800

    adding plane information

--- client/components/popup.jsx
+++ client/components/popup.jsx
@@ -1,8 +1,115 @@
 import React from 'react';
-
+import MyContext from './context';
 
 export default class PopUp extends React.Component{
   constructor(props){
     super(props);
   }
+
+  renderCountry(){
+    const flightCiao = this.props.flight;
+    if(this.context !==undefined){
+      const flag = this.context.map((values,i)=>{
+        if(values[0]===number){
+          if(values[2]==="United States"){
+            return <img key={i} className="flag" src="/images/usa.png" alt="American Flag"/>
+          }else if(values[2] === "Mexico"){
+            return <img key={i} className="flag" src="/images/mexico.png" alt="Mexican Flag" />
+          }
+        }
+      })
+      return flag;
+    }
+  }
+
+  planeInfo(){
+    if (this.context !== undefined) {
+      const test = this.context.map((values, i) => {
+        if (values[0] === this.props.flight) {
+          return (
+            <div className="panel" key={i}>
+              <div className="planeInfoRow">
+                <div className="col-12 planeInfoSection">
+                  <img className="airplaneLogo" src={airplane} />
+                  <div className="planeInfo">
+                    <p>Country of Reg.</p>
+                    {this.renderCountry()}
+                  </div>
+                  <div className="planeInfo">
+                    <p>Flight No.</p>
+                    <p>{values[1]}</p>
+                  </div>
+                  <div className="planeInfo">
+                    <p>Last Seen</p>
+                    <Moment className="size" unix tz="America/Los_Angeles">{values[3]}</Moment>
+                  </div>
+                </div>
+              </div>
+              <div className="col-12 planeInfoSection">
+                <img className="airplaneLogo" src={Gauge} />
+                <div className="planeInfo">
+                  <p>Altitude</p>
+                  <p>{Math.round(values[7] * 3.28084)}ft</p>
+                </div>
+                <div className="planeInfo">
+                  <p>Velocity</p>
+                  <p>{Math.round(values[9] * 2.23694)}mph</p>
+                </div>
+                <div className="planeInfo">
+                  <p>Vertical rate</p>
+                  <p>{Math.round(values[11] * 2.23694)}mph</p>
+                </div>
+              </div>
+              <div className="col-12 planeInfoSection">
+                <img className="airplaneLogo" src={Alt} />
+                <div className="planeInfo">
+                  <p>Latitude</p>
+                  <p>{values[6]}&deg; </p>
+                </div>
+                <div className="planeInfo">
+                  <p>Longitude</p>
+                  <p>{values[5]}&deg; </p>
+                </div>
+                <div className="planeInfo">
+                  <p>Track</p>
+                  <p>{values[10]}&deg; </p>
+                </div>
+              </div>
+              <div className="col-12 planeInfoSection">
+                <img className="airplaneLogo" src={Radar} />
+                <div className="planeInfo">
+                </div>
+                <div className="planeInfo">
+                  <p>ICAO 24-bit</p>
+                  <p>{values[0]}</p>
+                </div>
+                <div className="planeInfo">
+                  <p>Squawk</p>
+                  <p>{values[14]}</p>
+                </div>
+              </div>
+              <div className="col-12 planeInfoSection ">
+                <button>SAVE FLIGHT</button>
+              </div>
+            </div>
+
+          )
+        }
+      })
+      return test;
+    }
+  }
+  render() {
+    return (
+      <div className="pop">
+        <div className="row">
+          <div classname="column">
+            {this.testing()}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+PopUp.contextType = MyContext
